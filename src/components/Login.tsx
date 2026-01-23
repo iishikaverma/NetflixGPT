056a13190476473226373da0f69987765eb630ad
Ishika Verma <ishikaverma2028@gmail.com>
2026-01-24 01:17:52 +0530

    add sign-up form to login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+    const [isSignInForm, setIsSignInForm] = useState(true);
+
+    const toggleSignInForm = () =>{
+        setIsSignInForm(!isSignInForm);
+    }
+
     return(
         <div className="relative h-screen w-full">
             <Header/>
@@ -14,17 +21,24 @@ const Login = () => {
             <div className="relative z-10 flex justify-center items-center h-full">
                 <div className="w-[90%] sm:w-[380px] mt-5 bg-black/75 p-10 rounded-lg">
                     <h1 className="text-3xl font-bold text-white mb-6">
-                        Sign In
+                        {isSignInForm ? "Sign In" : "Sign Up"}
                     </h1>
                     <form className="flex flex-col gap-4">
-                        <input 
+
+                        {!isSignInForm && (<input 
                             type="text" 
+                            placeholder="Name" 
+                            className="h-12 rounded bg-zinc-800 px-4 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-600"
+                        />)}
+
+                        <input 
+                            type="email" 
                             placeholder="Email Address" 
                             className="h-12 rounded bg-zinc-800 px-4 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-600"
                         />
                         
                         <input 
-                            type="text" 
+                            type="password" 
                             placeholder="Password" 
                             className="h-12 rounded bg-zinc-800 px-4 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-600"
                         />
@@ -33,17 +47,19 @@ const Login = () => {
                             type="submit" 
                             className="mt-4 h-12 rounded bg-red-600 text-white font-semibold hover:bg-red-700 transition"
                         >
-                            Sign In
+                            {isSignInForm ? "Sign In" : "Sign Up"}
                         </button>
                         
-                        <p className="flex justify-center text-sm text-gray-400">OR</p>
+                        {isSignInForm && (<div>
+                            <p className="flex justify-center text-sm text-gray-400">OR</p>
 
-                        <button
-                            type="button"
-                            className="h-12 rounded bg-[#333] text-white hover:bg-[#444] transition"
-                        >
-                            Use a sign-in code
-                        </button>
+                            <button
+                                type="button"
+                                className="w-full mt-3 h-12 flex items-center justify-center rounded bg-[#333] text-white hover:bg-[#444] transition"
+                            >
+                                Use a sign-in code
+                            </button>
+                        </div>)}
                         <div>
                             <div className="text-sm text-gray-400">
                                 <span className="cursor-pointer hover:underline">
@@ -51,10 +67,13 @@ const Login = () => {
                                 </span>
                             </div>
 
-                            <p className="mt-4 text-sm text-gray-400">
-                                New to Netflix?{" "}
-                                <span className="cursor-pointer text-white hover:underline">
-                                    Sign up now
+                            <p className="mt-5 text-sm text-gray-400 hover:underline cursor-pointer">
+                                {isSignInForm ? "New to Netflix? " : "Already a member? "}
+                                <span
+                                className="cursor-pointer text-white hover:underline"
+                                onClick={toggleSignInForm}
+                                >
+                                    {isSignInForm ? "Sign up now" : "Sign in now"}
                                 </span>
                             </p>
                         </div>
