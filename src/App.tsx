4758a1b96d940d406276a2aa547b3fa293567e26
Ishika Verma <ishikaverma2028@gmail.com>
2026-01-23 17:28:06 +0530

    Initaial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-red-500">
+        Netflix
+      </h1>
+    </div>
+  );
+}
+
+export default App;
