ca20cf9dd0b8bc36653a45e38fd33d3e267e142c
Ishika Verma <ishikaverma2028@gmail.com>
2026-01-24 00:27:08 +0530

    add sign-in form to login page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,11 @@
 const Header = () => {
     return(
-        <div className="fixed top-0 left-0 w-full z-50 bg-gradient-to-b from-black">
-            <div className="flex items-center px-6 py-4 ">
-                <img
-                className="w-44"
-                src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-12-03/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-                alt="Netflix logo"
-                />
-            </div>
+        <div className="fixed top-0 left-0 z-50 w-full bg-gradient-to-b from-black px-4 sm:px-8 py-3 flex flex-col sm:flex-row justify-between items-center">
+            <img
+            className="w-24 sm:w-36 cursor-pointer py-1"
+            src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-12-03/consent/87b6a5c0-0104-4e96-a291-092c11350111/019ae4b5-d8fb-7693-90ba-7a61d24a8837/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
+            alt="Netflix logo"
+            />
         </div>
     )
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,10 +2,65 @@ import Header from "./Header";
 
 const Login = () => {
     return(
-        <div>
+        <div className="relative h-screen w-full">
             <Header/>
-            <img src = "https://assets.nflxext.com/ffe/siteui/vlv3/ce462eb6-4d7f-4c9a-9f61-93cb535a64fd/web/IN-en-20260105-TRIFECTA-perspective_5ec818ea-11f4-4bff-a409-8f36e9f9a1e2_medium.jpg"
-                alt = "Background"/>
+            <div>
+                <img src = "https://assets.nflxext.com/ffe/siteui/vlv3/ce462eb6-4d7f-4c9a-9f61-93cb535a64fd/web/IN-en-20260105-TRIFECTA-perspective_5ec818ea-11f4-4bff-a409-8f36e9f9a1e2_medium.jpg"
+                    alt = "Background"
+                    className="absolute inset-0 h-full w-full object-cover"
+                />
+            </div>
+
+            <div className="relative z-10 flex justify-center items-center h-full">
+                <div className="w-[90%] sm:w-[380px] mt-5 bg-black/75 p-10 rounded-lg">
+                    <h1 className="text-3xl font-bold text-white mb-6">
+                        Sign In
+                    </h1>
+                    <form className="flex flex-col gap-4">
+                        <input 
+                            type="text" 
+                            placeholder="Email Address" 
+                            className="h-12 rounded bg-zinc-800 px-4 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-600"
+                        />
+                        
+                        <input 
+                            type="text" 
+                            placeholder="Password" 
+                            className="h-12 rounded bg-zinc-800 px-4 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-600"
+                        />
+                        
+                        <button 
+                            type="submit" 
+                            className="mt-4 h-12 rounded bg-red-600 text-white font-semibold hover:bg-red-700 transition"
+                        >
+                            Sign In
+                        </button>
+                        
+                        <p className="flex justify-center text-sm text-gray-400">OR</p>
+
+                        <button
+                            type="button"
+                            className="h-12 rounded bg-[#333] text-white hover:bg-[#444] transition"
+                        >
+                            Use a sign-in code
+                        </button>
+                        <div>
+                            <div className="text-sm text-gray-400">
+                                <span className="cursor-pointer hover:underline">
+                                    Forgot password?
+                                </span>
+                            </div>
+
+                            <p className="mt-4 text-sm text-gray-400">
+                                New to Netflix?{" "}
+                                <span className="cursor-pointer text-white hover:underline">
+                                    Sign up now
+                                </span>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
