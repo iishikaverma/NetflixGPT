e47336b30704121604209e1f1ee5cc0b6bf6fdb4
Ishika Verma <ishikaverma2028@gmail.com>
2026-01-30 23:34:30 +0530

    made movie cards responsive across screen sizes

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -5,7 +5,7 @@ import { MovieCardProps } from "../utils/types"
 const MovieCard = ({posterPath}:MovieCardProps) => {
     if (!posterPath) return null;
     return (
-        <div className="w-44 flex-shrink-0 overflow-hidden">
+        <div className="w-28 flex-shrink-0 overflow-hidden sm:w-44">
             <img 
             className="rounded-md hover:scale-110 transition-transform duration-300 ease-out"
             alt="Movie Card" src={IMG_CDN + posterPath}/>
